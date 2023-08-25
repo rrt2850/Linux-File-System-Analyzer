b965984f9ac4d20bf1bae9e95635494f408a19ad
Robert Tetreault <rrt2850@g.rit.edu>
2023-08-25 17:08:47 -0400

    Base level implementation of directoryreader

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,26 @@
  *              coordinates between different classes.
  * Author: Robert Tetreault
  ******************************************************************************/
+
+#include <iostream>
+#include "DirectoryReader.h"
+
+int main() {
+    DirectoryReader reader("/mnt/c/");
+    reader.readDirectory();
+
+    std::vector<std::string> files = reader.getFiles();
+    std::vector<std::string> directories = reader.getDirectories();
+
+    std::cout << "Files:" << std::endl;
+    for (const auto& file : files) {
+        std::cout << file << std::endl;
+    }
+
+    std::cout << std::endl << "Directories:" << std::endl;
+    for (const auto& directory : directories) {
+        std::cout << directory << std::endl;
+    }
+
+    return 0;
+}
